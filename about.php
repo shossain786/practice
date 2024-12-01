2364b968132034cfa7a229790b9335183e93257c
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2024-12-01 22:16:25 +0530

    initial commit

--- about.php
+++ about.php
@@ -1,11 +1,19 @@
+<?php 
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php?page=login");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About</title>
+    <link rel="stylesheet" href="css/new_styles.css">
 </head>
 <body>
-    <?php include 'nav.pap'; ?>
+    <?php include 'nav.php'; ?>
 </body>
 </html>
